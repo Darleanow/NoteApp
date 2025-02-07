f82b2e34eeccb6893e8fd9eeb438cecc11b01af3
Enzo Hugonnier <enzohugonn@gmail.com>
2025-02-07 14:36:56 +0100

    refactor(Note): Rename to NoteEntity

--- app/src/main/java/com/esgi/noteapp/data/NoteDAO.kt
+++ app/src/main/java/com/esgi/noteapp/data/NoteDAO.kt
@@ -1,21 +1,22 @@
 package com.esgi.noteapp.data
 
 import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDAO {
     @Query("SELECT * FROM notes")
-    fun getAllNotes(): List<Note>
+    fun getAllNotes(): Flow<List<NoteEntity>>
 
     @Insert
-    fun insertNote(note: Note)
+    suspend fun insertNote(noteEntity: NoteEntity)
 
     @Update
-    fun updateNote(note: Note)
+    suspend fun updateNote(noteEntity: NoteEntity)
 
     @Delete
-    fun deleteNote(note: Note)
+    suspend fun deleteNote(noteEntity: NoteEntity)
 
     @Query("DELETE FROM notes")
-    fun deleteAllNotes()
+    suspend fun deleteAllNotes()
 }
--- app/src/main/java/com/esgi/noteapp/data/NoteEntity.kt
+++ app/src/main/java/com/esgi/noteapp/data/NoteEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes")
-data class Note(
+data class NoteEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     val id: Int = 0,
